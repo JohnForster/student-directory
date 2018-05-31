44a2f3d0efb0fbb986c0510ddc524cb3cfc96bc8
John Forster <j_forster@hotmail.co.uk>
2018-05-31 11:47:35 +0000

    End of step 4.

---
+++ directory.rb
@@ -0,0 +1,18 @@
+puts "The students of Villains Academy"
+puts "-------------"
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+students.each { |name| puts name }
+
+print "Overall, we have #{students.count} great students"
